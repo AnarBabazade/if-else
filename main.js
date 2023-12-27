0c36fe883ad4a506887571491163248ea80e05c6
AnarBabazade <babazadeanar94@gmail.com>
2023-12-28 03:37:53 +0400

    innersial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let a =prompt("ucbucagin birinci terefinin olcusunu yazin")
+let b =prompt("ucbucagin ikinci terefinin olcusunu yazin")
+let c =prompt("ucbucagin ucuncu terefinin olcusunu yazin")
+ if(a==b && b==c){
+        console.log("bu ucbucaq berabertereflidir");
+    }else if(a==b || b==c ||a==c){
+        console.log("bu ucbucaqda iki teref bir birine beraberdir");
+    }else{
+        console.log("bu ucbucaqda butun terefler ferqlidir");
+    }
+
+
